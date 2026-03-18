c791206f88b9b06a4bb4761cb9ece352995cc33b
robpacey32 <143297575+robpacey32@users.noreply.github.com>
2026-03-18 22:11:49 +0000

    Allow enter to confirm password

--- app/app.py
+++ app/app.py
@@ -15,10 +15,12 @@ if st.session_state.user is None:
     tab1, tab2 = st.tabs(["Login", "Register"])
 
     with tab1:
-        username = st.text_input("Username", key="login_username")
-        password = st.text_input("Password", type="password", key="login_password")
+        with st.form("login_form"):
+            username = st.text_input("Username", key="login_username")
+            password = st.text_input("Password", type="password", key="login_password")
+            login_submitted = st.form_submit_button("Login")
 
-        if st.button("Login"):
+        if login_submitted:
             user = login_user(username, password)
             if user:
                 st.session_state.user = {
@@ -26,19 +28,20 @@ if st.session_state.user is None:
                     "email": user["email"],
                     "user_id": user["username"],
                 }
-                if "display_currency" not in st.session_state:
-                    st.session_state.display_currency = "GBP"
+                st.session_state.display_currency = st.session_state.get("display_currency", "GBP")
                 st.success("Logged in")
                 st.rerun()
             else:
                 st.error("Invalid username or password")
 
     with tab2:
-        username = st.text_input("New username", key="reg_username")
-        email = st.text_input("Email", key="reg_email")
-        password = st.text_input("New password", type="password", key="reg_password")
+        with st.form("register_form"):
+            username = st.text_input("New username", key="reg_username")
+            email = st.text_input("Email", key="reg_email")
+            password = st.text_input("New password", type="password", key="reg_password")
+            register_submitted = st.form_submit_button("Register")
 
-        if st.button("Register"):
+        if register_submitted:
             ok, msg = register_user(username, email, password)
             if ok:
                 st.success(msg)
